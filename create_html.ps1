e7ca2149ababd78d1d48df0f8c27f9e14022e803
Sasaki Keisuke <ksk.andr@gmail.com>
2020-04-02 23:51:58 +0900

    URL取得処理を関数化

--- create_html.ps1
+++ create_html.ps1
@@ -2,12 +2,23 @@ param(
     [string]$favorites_path    # 処理対象パス(ディレクトリでもファイル名でも受け付ける)
 )
 
-function script:Main($favorites_path) {
+function script:getURLArray($favorites_path){
     $favorites = (Get-ChildItem -Recurse $favorites_path | Where-Object { $_.Attributes -ne "Directory" -and $_.Extension -eq ".url" })
 
     foreach ($favorite in $favorites) {
-        $url = (get-content $favorite.fullname) -match "^URL=.+"
-        Write-Host $favorite.fullname $url
+        $tempVal = (get-content $favorite.fullname) -match "^URL=.+"
+        $url = $tempVal[0].Substring(4,$tempVal[0].Length-4)
+
+        $urlArray += ,@($favorite.fullname,$url)
+    }
+    return $urlArray
+}
+
+function script:Main($favorites_path) {
+    $urlArray = getUrlArray $favorites_path
+    
+    foreach($cols in $urlArray){
+        write-host $cols[0] $cols[1]
     }
 }
 
