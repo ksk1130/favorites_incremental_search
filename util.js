5defeb35d73c20c8fe7bdf3faeee1126b5032b28
Sasaki Keisuke <ksk.andr@gmail.com>
2020-05-01 00:39:49 +0900

    いったん完成

--- util.js
+++ util.js
@@ -1,11 +1,51 @@
-function searchFavorite(){
-    console.log(document.getElementById("searchWord").value);
+var resultSet = new Set();
 
-    console.log(document.getElementById("favoritesList"));
+function narrowFavorites(searchWord) {
+  var parentNode = document.getElementById("resultArea");
+  parentNode.innerHTML = "";
 
-    document.getElementById("resultArea").innerHTML = document.getElementById("searchWord").value;
+  resultSet.forEach(function (value) {
+    console.log(value);
+    if (value.indexOf(searchWord) > 0) {
+      var li = document.createElement("li");
+      li.innerHTML = value;
+
+      parentNode.appendChild(li);
+    }
+  });
 }
 
-function clearResult(){
-    document.getElementById("resultArea").innerHTML = "";
+function searchFavorite() {
+  var searchWord = document.getElementById("searchWord").value;
+  console.log(searchWord);
+
+  if (searchWord == "") {
+    clearResult();
+    return;
+  }
+
+  if (resultSet.size > 0) {
+    narrowFavorites(searchWord);
+    return;
+  }
+
+  var lis = document.getElementById("favoritesList").getElementsByTagName("li");
+
+  for (var i = 0; i < lis.length; i++) {
+    var li = lis[i];
+    var a = li.firstChild;
+
+    var line = a.href + "\t" + a.innerHTML;
+
+    if (line.indexOf(searchWord) > 0) {
+      resultSet.add(line);
+    }
+  }
+
+  narrowFavorites(searchWord);
+}
+
+function clearResult() {
+  document.getElementById("resultArea").innerHTML = "";
+  resultSet.clear();
 }
